3eb07fd3bbfd060b8f84facece7086780426ca7f
Cedric <cedric.greutmann@vodafone.com>
2022-02-25 15:06:52 +0000

    wip added yaml

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello fhhhjjrom my laptop'),
+        body: JSON.stringify('Hello from my laptop'),
     };
     return response;
 };
