4576f746464078369f654606362891cc631a1a9c
Cedric <cedric.greutmann@vodafone.com>
2022-02-25 15:20:05 +0000

    updated yaml to give aws access

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello fhhhjjrom my laptop'),
+        body: JSON.stringify('Hello again from my github account'),
     };
     return response;
 };
